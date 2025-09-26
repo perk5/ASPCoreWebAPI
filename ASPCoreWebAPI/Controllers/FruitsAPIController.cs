f634389255fb8206891074729fd7ee97b0035a5c
Prerak Shah <prerak9359@gmail.com>
2025-09-27 00:51:47 +0930

    Working with fruitsAPI

---
+++ ASPCoreWebAPI/Controllers/FruitsAPIController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPCoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FruitsAPIController : ControllerBase
+    {
+        public List<string> fruits = new List<string>()
+        {
+            "Apple",
+            "Mango",
+            "Banana",
+            "Pear",
+            "Raspberry"
+        }; 
+
+        [HttpGet]
+        public List<string> GetFruits()
+        {
+            return fruits;
+        }
+
+        [HttpGet("{id}")]
+        public string GetFruitsByIndex(int id)
+        {
+            return fruits[id];
+        }
+    }
+}
